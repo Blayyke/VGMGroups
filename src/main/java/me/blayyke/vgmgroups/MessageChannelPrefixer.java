37172527572ca1b31ed31306a084fbabcd68bd2d
Blake Redenius <blakeredenius@gmail.com>
2018-04-19 20:11:47 +1000

    Add chat prefixes.

---
+++ src/main/java/me/blayyke/vgmgroups/MessageChannelPrefixer.java
@@ -0,0 +1,36 @@
+package me.blayyke.vgmgroups;
+
+import me.blayyke.vgmgroups.manager.GroupManager;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.channel.MessageChannel;
+import org.spongepowered.api.text.channel.MessageReceiver;
+import org.spongepowered.api.text.chat.ChatType;
+import org.spongepowered.api.text.format.TextColors;
+
+import javax.annotation.Nullable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+public class MessageChannelPrefixer implements MessageChannel {
+    @Override
+    public Collection<MessageReceiver> getMembers() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Optional<Text> transformMessage(@Nullable Object sender, MessageReceiver recipient, Text original, ChatType type) {
+        Player player = (Player) sender;
+        Optional<Group> playerGroup = GroupManager.getInstance().getPlayerGroup(player);
+        if (!playerGroup.isPresent()) return Optional.of(original);
+        Group group = playerGroup.get();
+
+        Text text = Text.builder()
+                .append(Text.of(TextColors.GREEN, "[" + group.getRank(player).getRank().getChatPrefix()))
+                .append(Text.of(TextColors.GREEN, group.getName() + "] "))
+                .append(original)
+                .toText();
+        return Optional.of(text);
+    }
+}
---
+++ src/main/java/me/blayyke/vgmgroups/listener/ChatListener.java
@@ -0,0 +1,19 @@
+package me.blayyke.vgmgroups.listener;
+
+import me.blayyke.vgmgroups.MessageChannelPrefixer;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.filter.cause.Root;
+import org.spongepowered.api.event.message.MessageChannelEvent;
+import org.spongepowered.api.text.channel.MessageChannel;
+
+public class ChatListener {
+    private MessageChannelPrefixer prefixerChannel = new MessageChannelPrefixer();
+
+    @Listener
+    public void onChat(MessageChannelEvent.Chat event, @Root Player player) {
+        MessageChannel originalChannel = event.getOriginalChannel();
+        MessageChannel newChannel = MessageChannel.combined(originalChannel, prefixerChannel);
+        event.setChannel(newChannel);
+    }
+}
