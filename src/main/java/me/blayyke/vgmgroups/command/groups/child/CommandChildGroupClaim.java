7bb4b466b73c47a0a0de3eaf579d06cac3b4b302
Blake Redenius <blakeredenius@gmail.com>
2018-04-25 07:56:01 +1000

    Create /group claim.

---
+++ src/main/java/me/blayyke/vgmgroups/command/groups/child/CommandChildGroupClaim.java
@@ -0,0 +1,53 @@
+package me.blayyke.vgmgroups.command.groups.child;
+
+import com.google.common.collect.Lists;
+import me.blayyke.vgmgroups.Group;
+import me.blayyke.vgmgroups.VGMGroups;
+import me.blayyke.vgmgroups.command.Command;
+import me.blayyke.vgmgroups.manager.GroupManager;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+
+import javax.annotation.Nonnull;
+import java.util.Optional;
+
+public class CommandChildGroupClaim extends Command {
+    public CommandChildGroupClaim(VGMGroups plugin) {
+        super(plugin, Lists.newArrayList("create"), Text.of("Create a group."));
+    }
+
+    @Nonnull
+    @Override
+    protected String getPermission() {
+        return "create";
+    }
+
+    @Override
+    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+        Player player = playersOnly(src);
+        Location<World> location = player.getLocation();
+        Group group = requireGroup(player);
+        Group groupForChunk = GroupManager.getInstance().getGroupForChunk(location.getExtent(), location.getChunkPosition());
+
+        if (groupForChunk == null) {
+            claimChunk(location);
+            return CommandResult.success();
+        }
+        if (group.equals(groupForChunk)) {
+            player.sendMessage(Text.of("Your group already owns this chunk."));
+            return CommandResult.success();
+        }
+
+        error(Text.of("This chunk is claimed by " + groupForChunk.getName() + "!"));
+        return CommandResult.success();
+    }
+
+    private void claimChunk(Location<World> location) {
+    }
+}
