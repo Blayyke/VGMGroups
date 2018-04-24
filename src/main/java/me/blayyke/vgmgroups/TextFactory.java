c4dcff572a55cc43d66f80c59913f9751606aa3b
Blake Redenius <blakeredenius@gmail.com>
2018-04-24 23:50:22 +1000

    Class to make text with newlines more efficiently.

---
+++ src/main/java/me/blayyke/vgmgroups/TextFactory.java
@@ -0,0 +1,22 @@
+package me.blayyke.vgmgroups;
+
+import org.spongepowered.api.text.Text;
+
+public class TextFactory {
+    public static Text createNewLineText() {
+        return Text.of("\n");
+    }
+
+    public static Text createTextSpanLines(String... lines) {
+        if (lines == null) return null;
+        if (lines.length < 1) return null;
+
+        Text.Builder builder = Text.builder();
+
+        for (String line : lines) {
+            builder.append(Text.of(line)).append(createNewLineText());
+        }
+
+        return builder.build();
+    }
+}
