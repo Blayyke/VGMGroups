4d60461eb6d54434a595242bcdaf35389109bb29
Blake Redenius <blakeredenius@gmail.com>
2018-04-24 23:50:46 +1000

    Start work on alerting when players enter another claim.

---
+++ src/main/java/me/blayyke/vgmgroups/listener/MoveListener.java
@@ -0,0 +1,37 @@
+package me.blayyke.vgmgroups.listener;
+
+import me.blayyke.vgmgroups.Group;
+import me.blayyke.vgmgroups.manager.GroupManager;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.entity.MoveEntityEvent;
+import org.spongepowered.api.event.filter.cause.Root;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.world.Chunk;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+
+import java.util.Optional;
+
+public class MoveListener {
+    @Listener
+    public void onMove(MoveEntityEvent event, @Root Player player) {
+        Optional<Group> groupOpt = GroupManager.getInstance().getPlayerGroup(player);
+        if (!groupOpt.isPresent()) return;
+        Group group = groupOpt.get();
+
+        Location<World> oldLocation = event.getFromTransform().getLocation();
+        Location<World> newLocation = event.getToTransform().getLocation();
+        World world = oldLocation.getExtent();
+
+        Optional<Chunk> oldChunk = world.getChunk(oldLocation.getChunkPosition());
+        Optional<Chunk> newChunk = world.getChunk(newLocation.getChunkPosition());
+        if (!oldChunk.isPresent()) return;
+        if (!newChunk.isPresent()) return;
+
+        boolean equals = newChunk.get().equals(oldChunk.get());
+        if (!equals) {
+            player.sendMessage(Text.of("You have entered a new chunk!"));
+        }
+    }
+}
