143eced4d87e76d372e0ff39c522b5166c58ab46
Blake Redenius <blakeredenius@gmail.com>
2018-05-06 22:12:15 +1000

    Change MessageChannelRelationship to a MutableMessageChannel.

--- src/main/java/me/blayyke/vgmgroups/channel/MessageChannelRelationship.java
+++ src/main/java/me/blayyke/vgmgroups/channel/MessageChannelRelationship.java
@@ -1,21 +1,41 @@
 package me.blayyke.vgmgroups.channel;
 
 import me.blayyke.vgmgroups.Group;
-import org.spongepowered.api.text.channel.MessageChannel;
 import org.spongepowered.api.text.channel.MessageReceiver;
+import org.spongepowered.api.text.channel.MutableMessageChannel;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.WeakHashMap;
 
-public class MessageChannelRelationship implements MessageChannel {
+public class MessageChannelRelationship implements MutableMessageChannel {
     private final Group group;
+    private Set<MessageReceiver> members;
 
     public MessageChannelRelationship(Group group) {
         this.group = group;
+        this.members = Collections.newSetFromMap(new WeakHashMap<>());
+        members.addAll(group.getOnlineMembers());
     }
 
     @Override
     public Collection<MessageReceiver> getMembers() {
-        return new ArrayList<>(group.getOnlineMembers());
+        return Collections.unmodifiableSet(members);
+    }
+
+    @Override
+    public boolean addMember(MessageReceiver member) {
+        return members.add(member);
+    }
+
+    @Override
+    public boolean removeMember(MessageReceiver member) {
+        return members.remove(member);
+    }
+
+    @Override
+    public void clearMembers() {
+        members.clear();
     }
 }
