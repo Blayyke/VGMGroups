0d441bddb8cfa1f9535aa6c2b0e1fa554f04464a
Blake Redenius <blakeredenius@gmail.com>
2018-05-07 09:20:45 +1000

    GroupEvent returns cause, make getters final.

--- src/main/java/me/blayyke/vgmgroups/event/GroupCreateEvent.java
+++ src/main/java/me/blayyke/vgmgroups/event/GroupCreateEvent.java
@@ -4,13 +4,8 @@ import me.blayyke.vgmgroups.Group;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.cause.Cause;
 
-public class GroupCreateEvent extends GropEvent {
-    public GroupCreateEvent(Player source, Group group) {
-        super(source, group);
-    }
-
-    @Override
-    public Cause getCause() {
-        return null;
+public class GroupCreateEvent extends GroupEvent {
+    public GroupCreateEvent(Player source, Group group, Cause cause) {
+        super(source, group, cause);
     }
 }
--- src/main/java/me/blayyke/vgmgroups/event/GroupEvent.java
+++ src/main/java/me/blayyke/vgmgroups/event/GroupEvent.java
@@ -2,24 +2,32 @@ package me.blayyke.vgmgroups.event;
 
 import me.blayyke.vgmgroups.Group;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.event.entity.living.humanoid.player.TargetPlayerEvent;
 import org.spongepowered.api.event.impl.AbstractEvent;
 
-public abstract class GropEvent extends AbstractEvent implements TargetPlayerEvent {
+public abstract class GroupEvent extends AbstractEvent implements TargetPlayerEvent {
     private final Player source;
     private final Group group;
+    private final Cause cause;
 
-    public GropEvent(Player source, Group group) {
+    public GroupEvent(Player source, Group group, Cause cause) {
         this.source = source;
         this.group = group;
+        this.cause = cause;
     }
 
-    public Group getGroup() {
+    public final Group getGroup() {
         return group;
     }
 
     @Override
-    public Player getTargetEntity() {
+    public final Player getTargetEntity() {
         return source;
+    }
+
+    @Override
+    public final Cause getCause() {
+        return cause;
     }
 }
