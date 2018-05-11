dca577799623b00282b72da6db4b3502f8d45206
Blake Redenius <blakeredenius@gmail.com>
2018-05-11 23:19:46 +1000

    Bump version.

--- src/main/java/me/blayyke/vgmgroups/Refs.java
+++ src/main/java/me/blayyke/vgmgroups/Refs.java
@@ -4,5 +4,5 @@ public class Refs {
     public static final String ID = "vgmgroups";
     static final String DESCRIPTION = "Plugin to manage player groups";
     public static final String NAME = "VGMGroups";
-    static final String VERSION = "0.3.11";
+    static final String VERSION = "0.4.0";
 }
